28e4750445c97f6e09725d3bb099264a3cf66660
Chad <chad.holzkamper@gmail.com>
2018-12-16 21:20:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // write your code below!
 
 function happyHolidays(string){
-   
+   return ('Happy Holidays!')
+}
+
+function happyHolidaysTo(string){
+  var holidayGreet = 'Happy holidays'
+  return '${holidayGreet}, ${name}!'
 }
